664e2401875e7e72014327953b7d4f99ef754e07
Nilabja Roy <nilabjar@gmail.com>
2014-11-27 12:50:21 -0600

    modified parenthesis

--- Recursion/parenthesis.cpp
+++ Recursion/parenthesis.cpp
@@ -1,7 +1,11 @@
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
 vector<string> sets;
+void getAllSets(int num);
+void getSet (int left, int right, string s, int position);
 
 int main (int argc, char* argv[])
 {
@@ -9,31 +13,47 @@ int main (int argc, char* argv[])
 
   getAllSets (num);
 
+  for (int i =0;i<sets.size();i++)
+  {
+    cout << sets[i] << endl; 
+  }
+
 }
 
 void getAllSets(int num)
 {
   string str;
-
   getSet (num, num, str, 0);
 }
 
 void getSet (int left, int right, string s, int position) {
 
+  if (left < 0 || right < left)
+    return;
+  cout << "Inside getset " << s << endl; 
+  cout << "right " << right << endl; 
+  cout << "left " << left << endl; 
   if (left == 0 && right == 0)
   {
+    cout << "Adding " << s << endl; 
     sets.push_back(s);
+    return;
   }
 
   if (left > 0)
   {
-    s[position] = "(";
-    getSet (left-1, right, s, position+1);
+    s.insert(position,1, '(');
+    cout << "s is " << s << endl; 
+    cout << "Adding left paren " << left << endl; 
+    getSet (left-1, right, s, position);
   }
 
-  if (right < left)
+  if (right > left)
   {
-    s[position] = ")";
+    cout << "s is " << s << endl; 
+    cout << "position is " << position << endl; 
+    s.insert(position,1, ')');
+    cout << "Adding right paren " <<  right << endl; 
     getSet (left, right-1, s, position+1);
   }
 }
