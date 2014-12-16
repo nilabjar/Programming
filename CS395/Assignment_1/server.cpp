c3a18510a461a54979c318b6d9948d5bd779b8ed
Nilabja Roy <nilabjar@nospam.email>
2014-12-16 16:25:54 -0600

    made the server multithreaded

--- CS395/Assignment_1/server.cpp
+++ CS395/Assignment_1/server.cpp
@@ -35,27 +35,36 @@ split (const char* string, char splitter)
 
 void* client_respond(void* conn_details)
 {
-    //get the conn socket
-    int connfd = (int)*conn_details;
+    char sendBuff[1024];
+    memset(sendBuff, '0', sizeof(sendBuff));
 
-    int n = read(connfd, sendBuff, sizeof(sendBuff));
+    //get the conn socket
+    int connfd = *((int*)conn_details);
+
+    int n = read( connfd, sendBuff, sizeof(sendBuff)-1 );
 
     sendBuff[n] = 0; // set the null value at the end
 
     //parse for the filename
-    char* p = strtok(sendBuffer, " ");
+    char* p = strtok(sendBuff, " ");
     p = strtok(NULL, " "); // this will get the second word ...filename
 
-    std::string s = p;
-    s.remove(0);
+    if (p == NULL) // no file name specified ..
+    {
+      cout << "No file name specified" << endl;
+      return 0;
+    }
 
-    ifstream in (s);
+    std::string s = p+1; // get rid of the first '/'
+
+    ifstream in (s.c_str());
+    cout << "Returning File " << s.c_str() << endl;
 
     while (in)
     {
       memset(sendBuff, 0 , sizeof(sendBuff));
-      in.read (sendBuff, sizeof(sendBuff));
-      write(connfd, sendBuff, strlen(sendBuff));
+      in.read ( sendBuff, sizeof(sendBuff)-1 );
+      write( connfd, sendBuff, strlen(sendBuff)-1 );
     }
 
     in.close();
@@ -67,11 +76,9 @@ int main (int argc, char* argv[])
 {
 
   struct sockaddr_in serv_addr;
-  char sendBuff[1024];
 
   int listenfd = socket(AF_INET, SOCK_STREAM, 0);
   memset(&serv_addr, '0', sizeof(serv_addr));
-  memset(sendBuff, '0', sizeof(sendBuff));
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -83,10 +90,11 @@ int main (int argc, char* argv[])
 
   while (true)
   {
+    cout << "Listening for incoming request" << endl;
     int connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 
     pthread_t thread;
-    pthread_create(thread, NULL, client_respond, (void*)&connfd);
+    pthread_create(&thread, NULL, client_respond, (void*)&connfd);
 
     sleep(1);
   }
